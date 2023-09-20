58fbda2f9483eaece8eb298f1e5c37193f0ea437
Phonethedor <alvaroc.giovannim@gmail.com>
2023-09-20 15:59:42 -0300

    base lista (sin login)

--- Sumativa2/sumativa2/inicio/urls.py
+++ Sumativa2/sumativa2/inicio/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='index'),
+    path('registro/', registro, name='registro')
 ]
--- Sumativa2/sumativa2/inicio/views.py
+++ Sumativa2/sumativa2/inicio/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'inicio/index.html')  
+
+def registro(request):
+    return render(request, 'inicio/registro.html')
