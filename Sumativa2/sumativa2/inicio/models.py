0e6258fc8cbe30c290524e5de7d2f9b430b8fda0
Phonethedor <alvaroc.giovannim@gmail.com>
2023-09-20 15:31:40 -0300

    añadido models

--- Sumativa2/sumativa2/inicio/models.py
+++ Sumativa2/sumativa2/inicio/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
 # Create your models here.
+
+# Propio de usuario
+class Rol(models.Model):
+
+    idRol = models.IntegerField(primary_key=True, verbose_name="Id rol de usuario")
+    nombreRol = models.CharField(max_length=10, verbose_name="nombre rol de usuario")
+
+    def __str__(self) -> str:
+        return self.nombre
+    
+class Usuario(models.Model):
+
+    idUsuario = models.AutoField(primary_key=True)
+    correoUsuario = models.CharField(max_length=50)
+    nombreUsuario = models.CharField(max_length=50)
+    passUsuario = models.CharField(max_length=50)
+    rolUsuario = models.ForeignKey(Rol ,on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.nombreUsuario
+    
+# Propio de producto
+class Proveedor(models.Model):
+    pass
+
+class Marca(models.Model):
+    pass
+
+class Categoria(models.Model):
+    pass
+
+class Tipo_producto(models.Model):
+    pass
+
+class Producto(models.Model):
+    pass
+
+# Propio de venta
+class Venta(models.Model):
+    pass
