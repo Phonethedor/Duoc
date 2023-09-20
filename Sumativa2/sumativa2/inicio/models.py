dfd6141e352bd2c581484fd21602c77c033b86f5
Phonethedor <alvaroc.giovannim@gmail.com>
2023-09-20 16:37:48 -0300

    models listo

--- Sumativa2/sumativa2/inicio/models.py
+++ Sumativa2/sumativa2/inicio/models.py
@@ -3,41 +3,100 @@ from django.db import models
 # Create your models here.
 
 # Propio de usuario
+
+# Rol usuario (1 = admin, 2 = user)
 class Rol(models.Model):
 
-    idRol = models.IntegerField(primary_key=True, verbose_name="Id rol de usuario")
-    nombreRol = models.CharField(max_length=10, verbose_name="nombre rol de usuario")
+    id_rol = models.IntegerField(primary_key=True, verbose_name="Id rol de usuario")
+    nombre_rol = models.CharField(max_length=10, verbose_name="nombre rol de usuario")
 
     def __str__(self) -> str:
-        return self.nombre
+        return self.nombre_rol
     
+# Tabla usuario
 class Usuario(models.Model):
 
-    idUsuario = models.AutoField(primary_key=True)
-    correoUsuario = models.CharField(max_length=50)
-    nombreUsuario = models.CharField(max_length=50)
-    passUsuario = models.CharField(max_length=50)
-    rolUsuario = models.ForeignKey(Rol ,on_delete=models.CASCADE)
+    id_usuario = models.AutoField(primary_key=True)
+    correo_usuario = models.CharField(max_length=50)
+    nombre_usuario = models.CharField(max_length=50)
+    pass_usuario = models.CharField(max_length=50)
+    rol_usuario = models.ForeignKey(Rol ,on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self) -> str:
-        return self.nombreUsuario
+        return self.nombre_usuario
     
 # Propio de producto
+
+# Quien nos vende los productos
 class Proveedor(models.Model):
-    pass
+    
+    id_proveedor = models.AutoField(primary_key=True)
+    nombre_proveedor = models.CharField(max_length=100)
 
+    def __str__(self) -> str:
+        return self.nombre_proveedor
+
+# Manufacturador de producto
 class Marca(models.Model):
-    pass
+    
+    id_marca = models.AutoField(primary_key=True)
+    nombre_marca = models.CharField(max_length=100)
 
+    def __str__(self) -> str:
+        return self.nombre_marca
+
+# categoria producto (gato, perro, medicina)
 class Categoria(models.Model):
-    pass
 
+    id_categoria = models.AutoField(primary_key=True)
+    nombre_categoria = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.nombre_categoria
+
+# Tipo de producto (alimento, juguete) (para futura implementacion de funcion de filtrado)
 class Tipo_producto(models.Model):
-    pass
+    
+    id_tipo_producto = models.AutoField(primary_key=True)
+    descripcion_tipo_producto = models.CharField(max_length=100)
 
+    def __str__(self) -> str:
+        return self.descripcion_tipo_producto
+
+# Producto en cuestion (created_at es la fecha que fue creado el registro y updated_at es la fecha en que se modifica)
 class Producto(models.Model):
-    pass
+    
+    id_producto = models.AutoField(primary_key=True)
+    nombre_producto = models.CharField(max_length=50)
+    descripcion_producto = models.CharField(max_length=200)
+    valor_producto = models.IntegerField()
+    stock_producto = models.IntegerField()
+    imagen_producto = models.ImageField(upload_to="imagenes")
+    proveedor_producto = models.ForeignKey(Proveedor, on_delete=models.CASCADE)
+    marca_producto = models.ForeignKey(Marca, on_delete=models.CASCADE)
+    categoria_producto = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    tipo_producto = models.ForeignKey(Tipo_producto, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.nombre_producto
 
 # Propio de venta
+
+# Tabla que registra los movimientos individuales de productos para luego ser incorporados en el detalle
 class Venta(models.Model):
-    pass
+    
+    id_venta = models.AutoField(primary_key=True)
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    cantidad = models.IntegerField()
+    total = models.IntegerField()
+
+# Contiene la cantidad total de la venta y tambien la fecha en que se realizo
+class Detalle_venta(models.Model):
+    
+    id_detalle = models.AutoField(primary_key=True)
+    ventas = models.ForeignKey(Venta, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
