b41b6ecfbbe1c1d34c789b67216ea76f1ba2297c
Phonethedor <alvaroc.giovannim@gmail.com>
2023-10-02 20:49:24 -0300

    Modificacion a admin.py

--- Sumativa3/sumativa3/inicio/admin.py
+++ Sumativa3/sumativa3/inicio/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
-from .models import Rol
+from .models import *
 # Register your models here.
 # cree un superuser  user:admin  pass:1234 
 # admin@duocuc.cl
 
 admin.site.register(Rol)
+admin.site.register(Usuario)
+admin.site.register(Proveedor)
+admin.site.register(Marca)
+admin.site.register(Categoria)
+admin.site.register(Tipo_producto)
+admin.site.register(Producto)
+admin.site.register(Venta)
+admin.site.register(Detalle_venta)
